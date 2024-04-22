27d9d1ba8e6a3b7dff413dafc2dcd16e80fa2716
Dani <danigrpi@gmail.com>
2024-04-22 18:22:40 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+mi idea es hacer una aplicación donde puedas comprar libros de algún géneri sabiendo el precio de compra
---
+++ test/app_libros_test.dart
@@ -0,0 +1,8 @@
+import 'package:app_libros/app_libros.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
